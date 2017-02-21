dccd13c3d3add21f4084e88fad74e9ba5e98c02c
Le Thanh Hien <hienthanhle91@gmail.com>
2017-02-21 08:31:11 +0700

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+   protect_from_forgery with: :exception
+   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+
+  def help
+  
+  end
+
+  def about
+  end
+  def contact
+  end	
+
+
+end
